0ddd6ee407b09573183bf9a8f35b6a3dd1dd10a2
NNazarov <kolya.nazarov41@gmail.com>
2023-07-26 23:37:14 +0300

    fix "may be end 3"

--- ewm-stats-service/server/src/main/java/ru/practicum/visit/repository/StatsRepositoryJPA.java
+++ ewm-stats-service/server/src/main/java/ru/practicum/visit/repository/StatsRepositoryJPA.java
@@ -14,11 +14,10 @@ import java.util.List;
 public interface StatsRepositoryJPA extends JpaRepository<StatsHit, Long> {
 
     @Query("select new ru.practicum.visit.model.StatsResponse(s.app, s.uri, " +
-            "case when :unique = true then count(distinct s.ip) else count(s.ip) end) " +
+            "case when :unique = true then count(distinct s.ip) else count(s.ip) end over(partition by s.app, s.uri)) " +
             " from StatsHit as s " +
             "where s.timestamp between :start and :end " +
             "and (cast(:uris as integer) is null or s.uri in :uris) " +
-            "group by s.app, s.uri " +
             "order by case when :unique = true then count(distinct s.ip) else s end desc")
     List<StatsResponse> findByDate(@Param("start") LocalDateTime start,
                                    @Param("end") LocalDateTime end,
